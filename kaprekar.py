d4f3c7b43f292e145d67615a6c74e53124a9a53b
carlpome <ceposada@sura.com.co>
2019-10-23 18:47:21 -0500

    added code to kaprekar process for 4 digits, pending to fix with 0's when it happens

---
+++ kaprekar.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def get_biggest(num):
+    lista = list(str(num))
+    lista.sort()
+
+    letras = "".join(lista)
+
+    low = int(letras)
+    big = int(letras[::-1])
+    
+    return big, low
+
+
+def kaprekar_4_cifras(num):
+    
+    num_iters = 0
+    for i in range(10):
+        big, low = get_biggest(num)
+        substraction = big-low
+        #print(i, substraction)
+        num = substraction
+        num_iters +=1
+        if num == 6174:
+            break
+    return num_iters
+
+
+
+for i in range(1000, 9999):
+    print(i, " -> ",kaprekar_4_cifras(i))
